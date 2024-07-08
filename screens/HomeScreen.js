0b13c10453f1493516c0c7529f670ab9803a5a4c
jolene <sitienijolene@gmail.com>
2024-07-08 10:37:22 +0300

    add the promotions,comments and partners file to the shared folde.create a card using conditional rendering to render featured p,p and c. feature them using scroll view

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,64 @@
+import { useState } from 'react';
 import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+import { PROMORIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
+
+const FeaturedItem = ({ item }) => {
+    //this is for conditional rendering
+    if(item){
+        return(
+            <Card containerStyle={{padding: 0}}>
+                <Card.Image source={item.image}>
+                    <View 
+                        style={{
+                            justifyContent: 'center',
+                            flex: 1
+                         }}
+                    >
+                        <Text
+                            style={{
+                                color: 'white',
+                                textAlign: 'center',
+                                fontSize: 20
+                            }}
+                        >
+                            {item.name}
+                        </Text>
+
+                    </View>
+                </Card.Image>
+
+                <Text style={{ margin: 20}}>
+                    {item.description}
+                </Text>
+            </Card>
+        );
+    }
+    return<View/>
+}
 
 const HomeScreen = () => {
-    retutn (
-        <View>
-            <Text>Home Screen </Text>
-        </View>
+
+    const[ campsites, setCampsited ] = useState(CAMPSITES);
+    const [promotions, setPromotions] = useState(PROMOTIONS);
+    const [partners, setPartners] = useState(PARTNERS);
+
+    //a constant for the featured campsites object
+    const featCampsite = campsites.find((item) => item.featured);
+    //constants for holding featured partner and featured promotion
+    const featPromotion = promotions.find((item)=> item.featured);
+    const featPartner = partners.find((item)=> item.featured);
+
+    return (
+        //scrollview will be used to render our featured item cards 
+        <ScrollView>
+            <FeaturedItem item={featCampsite} />
+            <FeaturedItem item={featPromotion} />
+            <FeaturedItem item={featPartner} />
+        </ScrollView>
     );
 }
 
