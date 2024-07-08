b2718a284bdd94e23df61f97e8e5b33905592e47
jolene <sitienijolene@gmail.com>
2024-07-08 09:49:27 +0300

    create a home screen file

---
+++ screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const HomeScreen = () => {
+    retutn (
+        <View>
+            <Text>Home Screen </Text>
+        </View>
+    );
+}
+
+export default HomeScreen;
