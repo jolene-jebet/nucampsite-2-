6c98e72b04e59214bebc533bb35b1ebec420ff7e
jolene <sitienijolene@gmail.com>
2024-07-08 09:31:29 +0300

    NAVIGATION 1

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -2,8 +2,9 @@ import RenderCampsite from '../features/campsites/RenderCampsite';
 
 const CampsiteInfoScreen = ({ route }) => {
     const { campsite } = route.params;
-    
+
     return <RenderCampsite campsite={props.campsite} />
+    
 }
 
 export default CampsiteInfoScreen;
