863c7e4b7dfcd53371dc0d50a831b3627912464d
jolene <sitienijolene@gmail.com>
2024-07-08 08:52:39 +0300

    importing navigation container

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Main from './screens/MainComponent';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <Main />
+    <NavigationContainer>
+      <Main />
+    </NavigationContainer>
+    
   );
 }
 
