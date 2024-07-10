25df720dd1cb18bf3419603815ef15496b2efc39
jolene <sitienijolene@gmail.com>
2024-07-10 23:39:29 +0300

    create the contact info page

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,11 +1,20 @@
 import { ScrollView } from "react-native";
 import { Card } from "react-native-elements";
+import { Text } from "react-native";
 
 const ContactScreen = () => {
     return(
         <ScrollView>
-            <Card>
-                
+            <Card wrapperStyle={{margin: 20}}>
+                {/* component works same as text tag where you can put plain text */}
+                <Card.Title>Contact Information</Card.Title>
+                <Card.Divider/>
+                <Text >1 Nucamp Way</Text>
+                <Text > Seattle, WA 98001</Text>
+                <Text style={{marginBottom: 10}}> U.S.A.</Text>
+                <Text > Phone: 1-206-555-1234</Text>
+                <Text > Email: 1bC9T@example.com</Text>
+
             </Card>
         </ScrollView>
     );
