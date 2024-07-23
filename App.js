f66eef502bb7620b897d70d36872f12018029d2b
jolene <sitienijolene@gmail.com>
2024-07-23 17:12:04 +0300

    wrap contents of main component is provider

--- App.js
+++ App.js
@@ -2,12 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Main from './screens/MainComponent';
 import { NavigationContainer } from '@react-navigation/native';
-
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 export default function App() {
   return (
-    <NavigationContainer>
-      <Main />
-    </NavigationContainer>
+    // wrapping entire main component in provider to enable it to access store
+    <Provider store={store}>
+      <NavigationContainer>
+        <Main />
+      </NavigationContainer>
+    </Provider>
     
   );
 }
